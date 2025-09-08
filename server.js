55b68b518658c3ae1a40a6f74b28aa124ca1b117
Dsharma <369dsharma@gmail.com>
2025-09-08 10:44:55 +0530

    added health check route

--- server.js
+++ server.js
@@ -93,6 +93,11 @@ const seedData = async () => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok", message: "Backend is running 🚀" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   seedData();
