e50b0406b8d4ecf028c8bea97809219da5faaa25
Dsharma <369dsharma@gmail.com>
2025-09-08 11:47:03 +0530

    updated

--- server.js
+++ server.js
@@ -18,11 +18,12 @@ app.use(cors({
     "http://localhost:3000",   
     "http://localhost:5173"    
   ],
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 // Database connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ecommerce')
 .then(()=>{
