0bb40d488299bbe518e7e2d7abaa0d3820308ff3
Dsharma <369dsharma@gmail.com>
2025-09-08 12:20:38 +0530

    updated

--- server.js
+++ server.js
@@ -12,26 +12,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
+// CORS configuration
 app.use(cors({
-  origin: [
-    process.env.FRONTEND_URL,
-    "http://localhost:3000",
-    "http://localhost:5173"
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-   allowedHeaders: [
-    'Content-Type', 
-    'Authorization', 
-    'X-Requested-With',
-    'session-id',
-    'Accept',
-    'Origin',
-    'Cache-Control',
-  ],
-  exposedHeaders: ['session-id'],
-  optionsSuccessStatus: 200,
+    origin: "*",
 }));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
